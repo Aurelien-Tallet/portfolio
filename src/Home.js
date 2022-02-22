e6de349a574bab781ff7dbe4af5175d26f706ec1
atallet <atallet@veepee.com>
2022-02-22 10:47:52 +0100

    test ssh key

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,20 @@
 import React, { useEffect } from 'react';
 import './Style/Home.css';
-import Title  from './Title'
+import Title from './Title'
 
 
 
 
 const Home = () => {
     console.log('monter Home')
-    useEffect(()=>{
-       return () => console.log('démonter Home')
-    },[])
-    
+    useEffect(() => {
+        return () => console.log('démonter SSH test')
+    }, [])
+
     return (
         <div className="Home">
             <Title />
-                
+
 
         </div>
     )
